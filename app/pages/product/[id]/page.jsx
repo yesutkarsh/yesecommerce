9405497d35ce6a983a7b8aea4feb96038a021835
yesutkarsh <contactutkarshverma@gmail.com>
2024-08-11 20:26:36 +0530

    bugs resolved

--- app/pages/product/[id]/page.jsx
+++ app/pages/product/[id]/page.jsx
@@ -1,31 +1,8 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'next/navigation'
-import Card from '../components/card'
+import React from 'react'
+import Id from './Id'
+
 export default function page() {
-  const params = useParams()
-  const {id} = params
-
-
-  const [data, setData] = useState([])
-
-  const fetching = async()=>{
-    const data = await fetch(`https://yescommercedummy.vercel.app/search/id/${id}`)
-    const json = await data.json()
-    setData(json)
-  }
-  useEffect(()=>{fetching()},[])
   return (
-    <>
-    {data.length!==0? 
-
-      data.map((prod)=>{
-        return <Card title={prod.name} description={prod.description}/>
-      })
-    
-    :"Sorry, but we couldn't find the product you're looking for."}
-
-
-    </>
+    <Id/>
   )
 }
--- app/pages/product/components/card.jsx
+++ app/pages/product/components/card.jsx
@@ -3,7 +3,7 @@ import style from "./card.module.css"
 export default function Card(props) {
   return (
     <>
-    <div className={style.wrapper}>
+    <div key={props.productId} className={style.wrapper}>
 
     <div className={style.section1}>
         <img src="https://rukminim2.flixcart.com/image/850/1000/xif0q/t-shirt/t/e/7/s-oversized-beige-ironflex-original-imagkjashnhxktjg.jpeg?q=90&crop=false" alt="product Image" />
